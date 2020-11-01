3d471e577ed5a2436fa5e9a378d35428ffdd88e5
shalongteng <1131790681@qq.com>
2020-11-02 07:45:47 +0800

    c

--- area.cpp
+++ area.cpp
@@ -1,29 +1,28 @@
-
-#include <stdio.h>
-#include <iostream>
-using namespace std;
-int prime(int  m)
-{
-    for(int i = 2;i<m;i++)
-    {
-        if(m%i == 0)
-            return 0;
-    }
-    return 1;
-}
-int main()
-{
-    int n =0;
-    int result = prime(13);
-
-    for(int i =2;i<= 100;i++)
-    {
-        if(prime(i)){
-            n++;
-            printf("%6d",i);
-            if(n%10 == 0){
-                printf("\n");
-            }
-        }
-    }
-}
+//#include <stdio.h>
+//#include <iostream>
+//using namespace std;
+//int prime(int  m)
+//{
+//    for(int i = 2;i<m;i++)
+//    {
+//        if(m%i == 0)
+//            return 0;
+//    }
+//    return 1;
+//}
+//int main()
+//{
+//    int n =0;
+//    int result = prime(13);
+//
+//    for(int i =2;i<= 100;i++)
+//    {
+//        if(prime(i)){
+//            n++;
+//            printf("%6d",i);
+//            if(n%10 == 0){
+//                printf("\n");
+//            }
+//        }
+//    }
+//}
--- cmake-build-debug/CMakeFiles/C_Study.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/C_Study.dir/cmake_clean.cmake
@@ -33,6 +33,7 @@ file(REMOVE_RECURSE
   "CMakeFiles/C_Study.dir/schoolwork/第六章/追查车号.cpp.o"
   "CMakeFiles/C_Study.dir/homework/03c语言中的数据成分/谁考了第k名.cpp.o"
   "CMakeFiles/C_Study.dir/homework/04运算成分基础练习/第二个重复出现的数.cpp.o"
+  "CMakeFiles/C_Study.dir/seatwork/06/chickenRabbit_.cpp.o"
   "C_Study.pdb"
   "C_Study"
 )
