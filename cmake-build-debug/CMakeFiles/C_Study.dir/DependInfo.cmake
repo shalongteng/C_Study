689a9a1b5ccb7e86e1fdcc2ef7106e50c62516f6
yuyanan <yuyanan@handongkeji.com>
2020-11-23 15:38:07 +0800

    basic

--- cmake-build-debug/CMakeFiles/C_Study.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/C_Study.dir/DependInfo.cmake
@@ -1,55 +1,17 @@
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "C"
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_C
-  "/Users/shalongteng/ClionProjects_test/C_Study/homework/03c语言中的数据成分/数据类型.c" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/homework/03c语言中的数据成分/数据类型.c.o"
-  )
-set(CMAKE_C_COMPILER_ID "AppleClang")
-
-# The include file search paths:
-set(CMAKE_C_TARGET_INCLUDE_PATH
-  )
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/shalongteng/ClionProjects_test/C_Study/area.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/area.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/debug.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/debug.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/homework/01感性认识计算机程序/leapYear.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/homework/01感性认识计算机程序/leapYear.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/homework/01感性认识计算机程序/与7无关的数.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/homework/01感性认识计算机程序/与7无关的数.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/homework/01感性认识计算机程序/奇数求和.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/homework/01感性认识计算机程序/奇数求和.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/homework/01感性认识计算机程序/贝贝找晶晶.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/homework/01感性认识计算机程序/贝贝找晶晶.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/homework/01感性认识计算机程序/鸡兔同笼.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/homework/01感性认识计算机程序/鸡兔同笼.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/homework/02从现实问题到计算基础程序/1的个数.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/homework/02从现实问题到计算基础程序/1的个数.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/homework/02从现实问题到计算基础程序/三角形判断.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/homework/02从现实问题到计算基础程序/三角形判断.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/homework/02从现实问题到计算基础程序/数组逆序重放.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/homework/02从现实问题到计算基础程序/数组逆序重放.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/homework/02从现实问题到计算基础程序/点和正方形的关系.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/homework/02从现实问题到计算基础程序/点和正方形的关系.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/homework/02从现实问题到计算基础程序/计算三角形面积.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/homework/02从现实问题到计算基础程序/计算三角形面积.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/homework/03c语言中的数据成分/奥运奖牌计数.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/homework/03c语言中的数据成分/奥运奖牌计数.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/homework/03c语言中的数据成分/整数的个数.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/homework/03c语言中的数据成分/整数的个数.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/homework/03c语言中的数据成分/细菌的战争.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/homework/03c语言中的数据成分/细菌的战争.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/homework/03c语言中的数据成分/能被3，5，7整除的数.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/homework/03c语言中的数据成分/能被3，5，7整除的数.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/homework/03c语言中的数据成分/谁考了第k名.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/homework/03c语言中的数据成分/谁考了第k名.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/homework/04运算成分基础练习/第二个重复出现的数.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/homework/04运算成分基础练习/第二个重复出现的数.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/main.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/main.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/schoolwork/数据类型/温度转换.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/schoolwork/数据类型/温度转换.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/schoolwork/数据类型/逆序打印.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/schoolwork/数据类型/逆序打印.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/schoolwork/第一周/输出学号.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/schoolwork/第一周/输出学号.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/schoolwork/第五章/天数判断.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/schoolwork/第五章/天数判断.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/schoolwork/第五章/星期推算.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/schoolwork/第五章/星期推算.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/schoolwork/第五章/特征数判断.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/schoolwork/第五章/特征数判断.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/schoolwork/第六章/test.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/schoolwork/第六章/test.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/schoolwork/第六章/图形打印.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/schoolwork/第六章/图形打印.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/schoolwork/第六章/求和S.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/schoolwork/第六章/求和S.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/schoolwork/第六章/特征四位数.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/schoolwork/第六章/特征四位数.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/schoolwork/第六章/追查车号.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/schoolwork/第六章/追查车号.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/schoolwork/第四章/test.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/schoolwork/第四章/test.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/schoolwork/第四章/时刻转换.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/schoolwork/第四章/时刻转换.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/schoolwork/第四章/进制转换.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/schoolwork/第四章/进制转换.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/seatwork/06/chickenRabbit .cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/seatwork/06/chickenRabbit_.cpp.o"
-  "/Users/shalongteng/ClionProjects_test/C_Study/seatwork/06/hundred.cpp" "/Users/shalongteng/ClionProjects_test/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/seatwork/06/hundred.cpp.o"
+  "C:/Users/Gavin/CLionProjects/C_Study/homework/01basicExercises/IsLeapYear.cpp" "C:/Users/Gavin/CLionProjects/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/homework/01basicExercises/IsLeapYear.cpp.obj"
+  "C:/Users/Gavin/CLionProjects/C_Study/homework/01basicExercises/JingjingGoesOnADate.cpp" "C:/Users/Gavin/CLionProjects/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/homework/01basicExercises/JingjingGoesOnADate.cpp.obj"
+  "C:/Users/Gavin/CLionProjects/C_Study/homework/01basicExercises/chickenRabbit.cpp" "C:/Users/Gavin/CLionProjects/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/homework/01basicExercises/chickenRabbit.cpp.obj"
+  "C:/Users/Gavin/CLionProjects/C_Study/homework/01basicExercises/nothingWithSeven.cpp" "C:/Users/Gavin/CLionProjects/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/homework/01basicExercises/nothingWithSeven.cpp.obj"
+  "C:/Users/Gavin/CLionProjects/C_Study/homework/01basicExercises/sumOddNumber.cpp" "C:/Users/Gavin/CLionProjects/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/homework/01basicExercises/sumOddNumber.cpp.obj"
+  "C:/Users/Gavin/CLionProjects/C_Study/main.cpp" "C:/Users/Gavin/CLionProjects/C_Study/cmake-build-debug/CMakeFiles/C_Study.dir/main.cpp.obj"
   )
-set(CMAKE_CXX_COMPILER_ID "AppleClang")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
